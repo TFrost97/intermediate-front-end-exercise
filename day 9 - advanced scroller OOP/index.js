6df91433cc49c6e5072a411a642b4f9d80f0b47b
TFrost97 <47691921+TFrost97@users.noreply.github.com>
2019-10-07 19:14:19 +0200

    add scroll feature

---
+++ day 9 - advanced scroller OOP/index.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('hello world');
+
+    const rootElement = document.querySelector('#root');
+    const sections = document.querySelectorAll('section');
+    let currentSectionIndex = 0;
+    let isThrottled = false;
+
+    document.addEventListener('mousewheel', function(event) {
+
+        if (isThrottled) return;
+        isThrottled = true;
+
+        setTimeout(function() {
+            isThrottled = false;
+        }, 1000);
+
+        console.log('jestem w mouse wheel');
+        const direction = event.wheelDelta < 0 ? 1 : -1;
+        console.log(event.wheelDelta)
+        if (direction === 1) {
+            const isLastSection = currentSectionIndex === sections.length - 1;
+            if (isLastSection) return;
+        } else if (direction === -1) {
+            const firstSection = currentSectionIndex === 0;
+            if (firstSection) return;
+        }
+
+        currentSectionIndex = currentSectionIndex + direction;
+
+        console.log(currentSectionIndex);
+
+        sections[currentSectionIndex].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        })
+    })
+})
