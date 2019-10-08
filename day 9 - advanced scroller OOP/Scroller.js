9fb2f622258845a61824bc3c8e0a24847eea269e
TFrost97 <47691921+TFrost97@users.noreply.github.com>
2019-10-08 22:36:11 +0200

    add navigation to the scroller

--- day 9 - advanced scroller OOP/Scroller.js
+++ day 9 - advanced scroller OOP/Scroller.js
@@ -14,7 +14,8 @@ class Scroller {
         console.log(this.currentSectionIndex, 'current section index')
         this.isThrottled = false;
 
-        // test
+        this.drawNavigation();
+
     }
 
     isScrolledIntoView(el) {
@@ -55,9 +56,51 @@ class Scroller {
     }
 
     scrollToCurrentSection = () => {
+        this.selectActiveNavItem();
         this.sections[this.currentSectionIndex].scrollIntoView({
             behavior: 'smooth',
             block: 'start',
         })
     }
+
+    drawNavigation = () => {
+        this.navigationContainer = document.createElement('aside');
+        this.navigationContainer.setAttribute('class', 'scroller__navigation');
+        const list = document.createElement('ul');
+        this.sections.forEach((section, index) => {
+            const listItem = document.createElement('li');
+
+            listItem.addEventListener('click', () => {
+                this.currentSectionIndex = index;
+
+                this.scrollToCurrentSection();
+            })
+
+            list.appendChild(listItem);
+
+
+        })
+
+        this.navigationContainer.appendChild(list);
+
+        document.body.appendChild(this.navigationContainer);
+
+        this.selectActiveNavItem();
+
+    }
+    selectActiveNavItem = () => {
+        if (this.navigationContainer) { 
+
+            const navigationItems = this.navigationContainer.querySelectorAll('li');
+
+            navigationItems.forEach((item, index) => {
+                if (index === this.currentSectionIndex) {
+                    item.classList.add('active');
+                } else {
+                    item.classList.remove('active');
+                }
+            })
+        }
+    }
+
 }
